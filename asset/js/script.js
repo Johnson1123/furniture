4b526929eef4972d7b9652ec5d1ea5680e0f3e23
Johnson1123 <onifadejohnson2017@gmail.com>
2022-04-25 18:35:48 +0100

    contact page creation

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,6 +1,19 @@
 const close = document.getElementById("close");
-const bars = document.getElementById('bars');
+const bars = document.getElementById("bars");
 const smNav =  document.getElementById('smNav');
+
+// google Zoom effect variable
+const zoomPlus = document.getElementById("zoomPlus");
+const zoomMinus = document.getElementById("zoomMinus");
+const googleImage = document.images["googleImage"];
+
+// google Zoom in And Zoom Out
+zoomPlus.addEventListener("click", function(){
+     googleImage.classList.add("scale-up");
+})
+zoomMinus.addEventListener("click", function(){
+     googleImage.classList.remove("scale-up");
+})
 
 close.addEventListener('click', function(){
      smNav.style.display = "none";
@@ -12,3 +25,4 @@ bars.addEventListener("click", function(e){
      smNav.style.display = "block";
      bars.style.display = "none";
 })
+
